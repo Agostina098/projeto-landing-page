5cf056e7c1196a6775aa806e272d54f6b057195f
agostinha <agostinhasants43@gmail.com>
2025-08-26 10:02:39 -0400

    Versão Inicial

---
+++ indexix.js
@@ -0,0 +1,28 @@
+var setadireita = window.document.getElementById("seta-direita")
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById ("samantha")
+var bruna = window.document.getElementById ("bruna")
+var setaesquerda = window.document.getElementById("seta-esquerda")
+ 
+function RolarParadireita(){
+
+    leonardo.style = "display:none"
+    bruna.style = "display:flex"
+    setadireita.style = "display:none"
+    setaesquerda.style = "display:flex; margin-top:55px"
+}
+
+function RolarParaesquerda(){
+
+leonardo.style = "display:flex"
+bruna.style = "display:none"
+setadireita.style = "display:flex; margin-top:55px"
+setaesquerda.style = "display:none"
+}
+
+
+
+
+
+
+    
